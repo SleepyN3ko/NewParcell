62c56a44394206affb1ff62e1dbe5346a5e6b561
CHONG TII TAO GERALD TP <2003936H@student.tp.edu.sg>
2021-12-11 20:11:42 +0800

    added 4 entities

---
+++ Parcell/Shared/Domain/OrderDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcell.Shared.Domain
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public virtual User User { get; set; }
+        public virtual Product Product { get; set; }
+
+    }
+}
---
+++ Parcell/Shared/Domain/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcell.Shared.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+        public string Thumbnail { get; set; }
+        public int Price { get; set; }
+        public string P_category { get; set; }
+        public string Swap_description { get; set; }
+
+        public virtual User User { get; set; }
+
+    }
+}
