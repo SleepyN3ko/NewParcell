97817f7e990e4ea8b7205e473c8e6aa9d2f6c488
CHONG TII TAO GERALD TP <2003936H@student.tp.edu.sg>
2021-12-14 15:46:31 +0800

    finished all the 4 repositories

---
+++ Parcell/Server/IRepository/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Parcell.Shared.Domain;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcell.Server.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task Save(HttpContext httpContext);
+        IGenericRepository<Product> Products { get; }
+        IGenericRepository<Category> Categories { get; }
+        IGenericRepository<Order> Orders { get; }
+        IGenericRepository<OrderDetail> OrderDetails { get; }
+        IGenericRepository<PaymentDetail> PaymentDetails { get; }
+        IGenericRepository<ProductCategory> ProductCategories { get; }
+        IGenericRepository<Swap> Swaps { get; }
+        IGenericRepository<SwapProduct> SwapProducts { get; }
+        IGenericRepository<WishList> WishLists { get; }
+        IGenericRepository<WishListItem> WishListItems { get; }
+        IGenericRepository<User> Users { get; }
+
+
+    }
+}
---
+++ Parcell/Server/Repository/UnitOfWork.cs
@@ -0,0 +1,87 @@
+﻿using Parcell.Server.Data;
+using Parcell.Server.IRepository;
+using Parcell.Server.Models;
+using Parcell.Shared.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Parcell.Server.Repository;
+
+namespace CarRentalManagement.Server.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private IGenericRepository<User> _users;
+        private IGenericRepository<Category> _categories;
+        private IGenericRepository<Order> _orders;
+        private IGenericRepository<OrderDetail> _orderdetails;
+        private IGenericRepository<PaymentDetail> _paymentdetails;
+        private IGenericRepository<Product> _products;
+        private IGenericRepository<ProductCategory> _productcategories;
+        private IGenericRepository<Swap> _swaps;
+        private IGenericRepository<SwapProduct> _swapproducts;
+        private IGenericRepository<WishList> _wishlists;
+        private IGenericRepository<WishListItem> _wishlistitems;
+
+        private UserManager<ApplicationUser> _userManager;
+
+        public UnitOfWork(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+
+        public IGenericRepository<User> Users
+            => _users ??= new GenericRepository<User>(_context);
+        public IGenericRepository<Category> Categories
+            => _categories ??= new GenericRepository<Category>(_context);
+        public IGenericRepository<Order> Orders
+            => _orders ??= new GenericRepository<Order>(_context);
+        public IGenericRepository<OrderDetail> OrderDetails
+            => _orderdetails ??= new GenericRepository<OrderDetail>(_context);
+        public IGenericRepository<PaymentDetail> PaymentDetails
+            => _paymentdetails ??= new GenericRepository<PaymentDetail>(_context);
+        public IGenericRepository<Product> Products
+            => _products ??= new GenericRepository<Product>(_context);
+        public IGenericRepository<ProductCategory> ProductCategories
+            => _productcategories ??= new GenericRepository<ProductCategory>(_context);
+        public IGenericRepository<Swap> Swaps
+            => _swaps ??= new GenericRepository<Swap>(_context);
+        public IGenericRepository<SwapProduct> SwapProducts
+            => _swapproducts ??= new GenericRepository<SwapProduct>(_context);
+        public IGenericRepository<WishList> WishLists
+            => _wishlists ??= new GenericRepository<WishList>(_context);
+        public IGenericRepository<WishListItem> WishListItems
+            => _wishlistitems ??= new GenericRepository<WishListItem>(_context);
+
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save(HttpContext httpContext)
+        {
+            //To be implemented
+            string user = "System";
+
+            var entries = _context.ChangeTracker.Entries()
+                .Where(q => q.State == EntityState.Modified ||
+                    q.State == EntityState.Added);
+
+
+            await _context.SaveChangesAsync();
+
+
+        }
+    }
+}
