db2ce76cfe8d95f918c9ecc244e989195e291fbd
Sleepai <63096598+SleepyN3ko@users.noreply.github.com>
2022-01-13 13:59:45 +0800

    Updated Missing Image Handling

--- Parcell/Shared/Domain/Product.cs
+++ Parcell/Shared/Domain/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,27 @@ namespace Parcell.Shared.Domain
 {
     public class Product
     {
+
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100,MinimumLength = 5,ErrorMessage ="Product Name does not meet length requirements")]
         public string Name { get; set; }
         public string Image { get; set; }
+
+        [Required]
         public string Description { get; set; }
+        [Required]
         public int Stock { get; set; }
         public string Thumbnail { get; set; }
+
+        [Required]
         public int Price { get; set; }
+        [Required]
         public string P_category { get; set; }
         public string Swap_description { get; set; }
 
+        
         public string Username { get; set; }
 
     }
