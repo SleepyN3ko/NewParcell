ccbfaf77e7dbea67ffa4aadc362d4f8ba2d09a0e
Sleepai <snowderemusic@gmail.com>
2022-01-30 20:17:43 +0800

    Added Proper Error Handling

--- Parcell/Client/Program.cs
+++ Parcell/Client/Program.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Parcell.Client.Services;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 namespace Parcell.Client
 {
     public class Program
@@ -21,8 +22,18 @@ namespace Parcell.Client
             builder.Services.AddHttpClient("Parcell.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
+            builder.Services.AddHttpClient("CarRentalManagement.ServerAPI", (sp, client) => {
+                client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                client.EnableIntercept(sp);
+                })
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Parcell.ServerAPI"));
+
+            builder.Services.AddHttpClientInterceptor();
+
+            builder.Services.AddScoped<HttpInterceptorService>();
 
             builder.Services.AddApiAuthorization();
 
