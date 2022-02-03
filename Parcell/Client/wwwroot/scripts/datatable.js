a065cb835044e7e89e986ff266f16260cb27664e
Sleepai <snowderemusic@gmail.com>
2022-02-03 14:46:50 +0800

    Added custom filters for categories datatables

--- Parcell/Client/wwwroot/scripts/datatable.js
+++ Parcell/Client/wwwroot/scripts/datatable.js
@@ -1,7 +1,30 @@
 ﻿function AddDataTable(table) {
-	$(document).ready(function () {
-		$(table).DataTable();
-	})
+    $(document).ready(function () {
+        $(table).DataTable({
+            initComplete: function () {
+                this.api().columns([2,3]).every(function () {
+                    var column = this;
+                    var select = $('<select><option value=""></option></select>')
+                        .appendTo($(column.header()))
+                        .on('change', function () {
+                            var val = $.fn.dataTable.util.escapeRegex(
+                                $(this).val()
+                            );
+
+                            column
+                                .search(val ? '^' + val + '$' : '', true, false)
+                                .draw();
+                        });
+                    $(select).click(function (e) {
+                        e.stopPropagation();
+                    });
+                    column.data().unique().sort().each(function (d, j) {
+                        select.append('<option value="' + d + '">' + d + '</option>')
+                    });
+                });
+            }
+        });
+    });
 }
 function DataTablesDispose(table) {
 	$(document).ready(function () {
